5db6ea720f2c5e78e0357aa0004edee8dcb2aa44
Ishant singh <singhishant683@gmail.com>
2026-04-19 13:02:24 +0530

    Delete frontend directory

--- frontend/src/api/dashboard.js
+++
@@ -1,6 +0,0 @@
-import api from './axios';
-
-export const getDashboardOverview = async () => {
-  const response = await api.get('/dashboard/overview');
-  return response.data;
-};
--- frontend/src/api/programs.js
+++
@@ -1,6 +0,0 @@
-import api from './axios';
-
-export const getPrograms = async (params) => {
-  const response = await api.get('/programs', { params });
-  return response.data;
-};
